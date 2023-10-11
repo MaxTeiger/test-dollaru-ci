7effa5b0b80ecef07bf16f989c6f4165ad51c634
Max <max.teiger@gmail.com>
2023-10-11 12:17:20 +0200

    feat(ci): create packages for every modified node

--- nodes/node01/script-01.sh
+++ nodes/node01/script-01.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "Simple text"
+# Create a list
+list=(apple banana orange apple pear)
+
+# Add an element to the list
+list+=("grape")
+
+# Remove duplicates from the list
+unique_list=($(echo "${list[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+
+# Print the unique list
+echo "${unique_list[@]}"
+
+list=()
