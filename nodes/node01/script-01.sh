a7a6d48b45b338696c6eae403bea71a1920f7ad8
Max <max.teiger@gmail.com>
2023-10-23 09:00:57 +0200

    feat(script): modify script

--- nodes/node01/script-01.sh
+++ nodes/node01/script-01.sh
@@ -1,17 +1,26 @@
-#!/bin/bash
-set -euo pipefail
+echo "Commit SHA: $GIT_SHA"
 
-# Create a list
-list=(apple banana orange apple pear)
+folders=$(cd ./nodes && find . -mindepth 1 -maxdepth 1 -type d | cut -c 3-)
 
-# Add an element to the list
-list+=("grape")
+# Print the list of nodes names
+echo "${folders[@]}"
+for node in $folders; do
+    echo "<---------------- ${node} ---------------->"
+    echo "aws s3 ls \"s3://dollaru-script/$node/$GIT_SHA\""
+    
+    # if aws s3 ls "s3://dollaru-script/$node/$GIT_SHA" 2>&1 | grep -q 'NoSuchBucket\|NoSuchKey'; then
 
-# Remove duplicates from the list
-unique_list=($(echo "${list[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
-
-# Print the unique list
-echo "${unique_list[@]}"
-
-list=()
-# Modification to trigger ci
+    if false; then
+        echo "Folder does not exist"
+    else
+        echo "Folder exists in s3"
+        echo "🕦📦 Downloading package.tar.gz from S3"
+        echo "aws s3 cp s3://dollaru-script/$node/$GIT_SHA/package.tar.gz package.tar.gz"
+        echo "✅📦 Downloaded package.tar.gz from S3"
+        
+        echo "🕦🪣 Uploading package.tar.gz to s3 bucket"
+        echo "aws s3 cp package.tar.gz s3://dollaru-script/$node/$RELEASE_VERSION/package.tar.gz"
+        echo "✅🪣 Uploaded package.tar.gz to s3 bucket"
+        
+    fi
+done
