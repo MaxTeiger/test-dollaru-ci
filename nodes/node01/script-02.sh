4aa3bfa7bb292022bf40b7d69ae7e6d07c62ed50
Max <max.teiger@gmail.com>
2023-10-23 09:01:44 +0200

    feat(script): modify script

--- nodes/node01/script-02.sh
+++ nodes/node01/script-02.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "Simple text"
+bucket_name="your-bucket-name"
+folder_path="your-folder-path"
+
+if aws s3 ls "s3://$bucket_name/$folder_path" 2>&1 | grep -q 'NoSuchBucket\|NoSuchKey'; then
+	echo "Folder does not exist"
+else
+	echo "Folder exists"
+fi
+
+# Replace all occurrences of "\n" with ","
+folder_list=$(aws s3 ls "s3://$bucket_name/$folder_path" | awk '{print $2}' | tr '\n' ',' | sed 's/,$//')
+
+echo "Folder list: $folder_list"
+
+
+
+
